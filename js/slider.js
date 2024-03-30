bd70819c9cece0dbcafca973d5ab09a81a530bf0
Anik-Nath <anikdevnath03112000@gmail.com>
2024-03-31 04:11:51 +0600

    almost done

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $(".center").slick({
-      dots: true,
-      arrows:true,
-      infinite: true,
-      centerMode: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-    });
+  $(".center").slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   });
+});
