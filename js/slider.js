85f6f4e6a3bf8f19fc45425a7fa27d4398df966d
Anik-Nath <anikdevnath03112000@gmail.com>
2024-03-31 10:36:40 +0600

    done

--- js/slider.js
+++ js/slider.js
@@ -10,3 +10,19 @@ $(document).ready(function () {
     autoplaySpeed: 3000,
   });
 });
+
+const imageContainer = document.getElementById("location");
+const text = document.getElementById("location-text");
+const locationImage = document.getElementById("locationImage");
+let previousInnerText = text.innerText;
+
+imageContainer.addEventListener("mouseout", () => {
+  locationImage.src = "../assets/mapindicate.png";
+  text.innerText = previousInnerText;
+  locationImage.classList = "w-6";
+});
+imageContainer.addEventListener("mouseover", () => {
+  locationImage.src = "../assets/magnifier.png";
+  locationImage.classList = "w-10";
+  text.innerText = "bir sehir ara...";
+});
